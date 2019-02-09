f65da599da3efadccaa7f1d4f7582712114e3bf4
sharpkiss24 <shariana.allen@gmail.com>
2019-02-09 15:34:29 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var image = document.getElementsById("img1");
+
+videoBanner.addEventListener("click", myEventHandler);
+
+function myEventHandler(){
+	alert("what are you doing? do you know what your doing? if you respond, your talking to a computer")
+}
