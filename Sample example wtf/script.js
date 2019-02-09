1d5c2833b794b2871a99b710e115787b8e84a671
sharpkiss24 <shariana.allen@gmail.com>
2019-02-09 13:05:22 -0500

    Update Sample example wtf/script.js

--- Sample example wtf/script.js
+++ Sample example wtf/script.js
@@ -13,6 +13,12 @@ newHeader.innerHTML = "Ladies and Gents";
 videoBanner.insertBefore(newHeader, paragraph);
 
 //remove stuff//
-videoBanner.removeChild(paragraph);
+//videoBanner.removeChild(paragraph);
 
-videoBanner.removeChild(newHeader);
+//videoBanner.removeChild(newHeader);
+
+videoBanner.addEventlistener("click", myEventHandler);
+
+function myEventHandler() {
+	alert("hey Man don't go clicking random stuff if your bored")
+}
